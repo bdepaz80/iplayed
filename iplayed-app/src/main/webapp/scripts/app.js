7f53c920431c401aba42a41e454e73bf40a0a9ce
Bert Depaz <bert.depaz@gmail.com>
2016-02-08 20:31:12 +0100

    Raw statistics page.

--- iplayed-app/src/main/webapp/scripts/app.js
+++ iplayed-app/src/main/webapp/scripts/app.js
@@ -3,4 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-var app = angular.module('iplayed', []);
+(function() {
+    var app = angular.module('iplayed', []);
+    
+    app.controller('StatisticsController', function(){
+        this.statistics = statistics;
+    });
+    
+    var statistics = {"totalMatches":3,"totalSets":9,"totalPoints":97,"totalPlayers":3,"playerWins":{"entry":[{"key":{"username":"bdepaz","firstname":"Bert","lastname":"Depaz"},"value":2},{"key":{"username":"dfranssen","firstname":"Dirk","lastname":"Franssen"},"value":1}]}};
+})();
