46a2b99d3c78657e4c68793d0bde57f8a98dbec3
Bert Depaz <bert@depaz.be>
2016-02-22 20:38:10 +0100

    Initialize google chart.

--- iplayed-app/src/main/webapp/scripts/app.js
+++ iplayed-app/src/main/webapp/scripts/app.js
@@ -1,12 +1,17 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 (function() {
     var app = angular.module('iplayed', ['ngRoute', 'ngResource', 'iPlayedControllers']);
-    
-    app.config(['$routeProvider', 
+
+    google.setOnLoadCallback(function() {
+      angular.bootstrap(document.body, ['iplayed']);
+    });
+    google.load('visualization', '1', {packages: ['corechart']});
+
+    app.config(['$routeProvider',
         function($routeProvider) {
             $routeProvider.
                 when('/statistics', {
@@ -15,14 +20,15 @@
                 }).
                 when('/player', {
                     templateUrl: 'pages/player.html',
-                    controller: 'PlayerController'                        
+                    controller: 'PlayerController'
                 }).
                 when('/about', {
                     templateUrl: 'pages/about.html',
-                    controller: 'AboutController'                        
+                    controller: 'AboutController'
                 }).
                 otherwise({
                     redirectTo: 'statistics'
                 });
         }]);
+
 })();
--- iplayed-app/src/main/webapp/scripts/controllers.js
+++ iplayed-app/src/main/webapp/scripts/controllers.js
@@ -1,23 +1,85 @@
 var iPlayedControllers = angular.module('iPlayedControllers', []);
 
-iPlayedControllers.factory("Statistics", ['$resource', function($resource) {
-   return $resource("resources/statistics/:Username",
-        {Username: "@Username"}
-   ); 
-}]);
+iPlayedControllers.factory("Statistics", ['$resource', function ($resource) {
+        return $resource("resources/statistics/:Username",
+                {Username: "@Username"}
+        );
+    }]);
 
-iPlayedControllers.controller('StatisticsController', ['$scope', 'Statistics', function($scope, Statistics){
-    this.statistics = Statistics.get();
-    //this.statistics = statistics;
-}]);
+iPlayedControllers.controller('StatisticsController', ['$scope', 'Statistics', function ($scope, Statistics) {
+        $scope.statistics = Statistics.get();
+        //this.statistics = statistics;
 
-iPlayedControllers.controller('PlayerController', ['$scope', 'Player', function($scope, Player){
+        $scope.chartTitle = "My Daily Activities";
+        $scope.chartWidth = 500;
+        $scope.chartHeight = 320;
+    }]);
 
-}]);
+iPlayedControllers.directive('chart', function ($timeout) {
+    return {
+        restrict: 'EA',
+        scope: {
+            title: '@title',
+            width: '@width',
+            height: '@height',
+            data: '=data',
+            selectFn: '&select'
+        },
+        link: function ($scope, $elm, $attr) {
+            // Create the data table.
+            var data = new google.visualization.DataTable();
+            data.addColumn('string', 'Player');
+            data.addColumn('number', 'Victories');
 
-iPlayedControllers.controller('AboutController', ['$scope', 'About', function($scope, About){
+            var chart = new google.visualization.BarChart($elm[0]);
 
-}]);
+            draw();
 
+            $scope.$watch('data', function () {
+                draw();
+            }, true);
+            $scope.$watch('title', function () {
+                draw();
+            });
+            $scope.$watch('width', function () {
+                draw();
+            });
+            $scope.$watch('height', function () {
+                draw();
+            });
 
-var statistics = {"totalMatches":3,"totalSets":9,"totalPoints":97,"totalPlayers":3,"playerWins":{"entry":[{"key":{"username":"bdepaz","firstname":"Bert","lastname":"Depaz"},"value":2},{"key":{"username":"dfranssen","firstname":"Dirk","lastname":"Franssen"},"value":1}]}};
+            function draw() {
+                if (!draw.triggered) {
+                    draw.triggered = true;
+                    $timeout(function () {
+                        draw.triggered = false;
+                        var label, value;
+                        data.removeRows(0, data.getNumberOfRows());
+                        angular.forEach($scope.data, function (row) {
+                            label = row.key.username;
+                            value = row.value;
+                            if (!isNaN(value)) {
+                                data.addRow([label, value]);
+                            }
+                        });
+                        var options = {'title': $scope.title,
+                            'width': $scope.width,
+                            'height': $scope.height};
+                        chart.draw(data, options);
+                        // No raw selected
+                        $scope.selectFn({selectedRowIndex: undefined});
+                    }, 0, true);
+                }
+            }
+            ;
+        }
+    }
+});
+
+iPlayedControllers.controller('PlayerController', ['$scope', 'Player', function ($scope, Player) {
+
+    }]);
+
+iPlayedControllers.controller('AboutController', ['$scope', 'About', function ($scope, About) {
+
+    }]);
